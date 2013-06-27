b9670be449a2334cb7b36d8b23de5b43485b303a
Amarnath <amarnath.bagineni@costrategix.com>
2013-06-27 15:13:35 +0530

    started off with the edit page (only title input included) ... complete thies .. and "Validate.js" is still pending

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -15,4 +15,31 @@ class Home extends CI_Controller {
                 );
         $this->load->view('layout/content_layout',$data);        
     }
+    
+    function blockForm($action, $id) {
+       
+       /* 
+        if(($action == 'contribute')&&($id == 0)) {
+            $pageDataArray = array(); // change this repeated declaration
+        } else */ 
+            $pageDataArray = array();
+            $pageDataArray['action'] = $action;
+            if($action == 'edit') {
+            
+            //need to populate this array with the id values 
+            //
+            //$this->load->model('block_manager');
+            //$pageDataArray['dbData'] = $this->account_manager->getsomedata($id);
+              
+              $pageDataArray['id'] = $id;
+        } else if ($action == 'register') {
+              $pageDataArray['id'] = 0;
+        }
+        
+        $data = array(
+                'pageLoad' => 'block_form',
+                'pageData' => $pageDataArray
+                );
+        $this->load->view('layout/content_layout',$data);        
+    }
 }
---
+++ application/models/block_manager.php
@@ -0,0 +1,11 @@
+<?php
+
+class Account_manager extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+    
+    
+    
+}
